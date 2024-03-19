2f5d198ef9e1d1b847571363ca5e726e526575e8
Truong Huu Tai <taitruong1302@gmail.com>
2024-03-19 11:04:33 +0700

    edit config db to deploy

--- src/config/connectDatabase.js
+++ src/config/connectDatabase.js
@@ -1,9 +1,15 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('apartment_rental', 'root', '1234', {
-    host: 'localhost',
-    dialect: 'mysql',
-    logging: false
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+    logging: false,
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    }
 });
 
 const connectDB = async () => {
